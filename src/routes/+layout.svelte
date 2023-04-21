72ea2ce6e56570d8f97c999caabfae765bdfea1c
Mike Repec <mikerepec@gmail.com>
2023-04-21 21:06:55 +0200

    Setup simple search form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import { enhance } from '$app/forms';
 	import { invalidate } from '$app/navigation';
+	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import type { LayoutData, SubmitFunction } from './$types';
 
@@ -38,16 +39,36 @@
 <div class="mx-auto w-[1200px] max-w-full transition-opacity duration-700 opacity-100">
 	<div class="p-6">
 		<div class="mb-5 flex justify-between">
-			<h1 class="text-3xl font-bold">🍿 GoodFlix</h1>
+			<h1 class="text-3xl font-bold">
+				<a href="/">🍿 GoodFlix</a>
+			</h1>
 
-			<div>
+			<div class="flex items-center gap-2">
 				{#if session}
+					<a
+						href="/search"
+						class="button primary link"
+						aria-current={$page.url.pathname === '/search'}
+					>
+						🎬 Add Movie
+					</a>
 					<form method="post" action="/?/signout" use:enhance={handleSignout}>
-						<button
-							class="button block p-2 rounded hover:bg-slate-200 disabled:opacity-30"
-							disabled={signingOut}
-						>
-							Sign Out
+						<button class="button" disabled={signingOut}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+								/>
+							</svg>
+							<span>Sign Out</span>
 						</button>
 					</form>
 				{/if}
@@ -59,3 +80,9 @@
 
 	<footer class="p-6 text-center text-xs text-gray-500">Powered by ☕️ and 🍕</footer>
 </div>
+
+<style lang="postcss">
+	.link[aria-current='true']:not(:hover) {
+		@apply bg-yellow-200;
+	}
+</style>
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -6,7 +6,27 @@
 	$: ({ query, results, error } = data);
 </script>
 
-<h1>Search</h1>
+<h1 class="text-xl font-bold">Watch anything good recently?</h1>
+
+<form class="w-1/2 m-auto my-12 flex items-center gap-1">
+	<input type="text" name="q" id="query" required value={query} class="form-input" />
+	<button class="button">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+			/>
+		</svg>
+	</button>
+</form>
 
 {#if query}
 	<p>Results for: {query}</p>
