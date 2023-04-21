3243f2b49056bbaa6ba791dae96d8a9e89748e25
Mike Repec <mikerepec@gmail.com>
2023-04-21 23:19:49 +0200

    Improve movies api caching and image url formatting

--- src/lib/server/movies-api.ts
+++ src/lib/server/movies-api.ts
@@ -3,9 +3,9 @@ import type { Movie, MovieSearchResults } from '../types/movie';
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
-const cache: Record<string, unknown> = {};
+const cache: Record<string, string> = {};
 
-export interface ApiResults<T> {
+export interface ApiResult<T> {
 	data?: T;
 	error?: ApiError;
 }
@@ -15,34 +15,36 @@ export interface ApiError {
 	status_code: number;
 }
 
+interface ImagePaths {
+	poster_path: string | null;
+	backdrop_path: string | null;
+}
+
 export class MoviesApi {
 	constructor(private apiKey: string) {}
 
-	async getMovie(id: number): Promise<ApiResults<Movie>> {
+	async getMovie(id: number): Promise<ApiResult<Movie>> {
 		const result = await this.get<Movie>(`${API_BASE_URL}/movie/${id}`);
 		if (result.data) {
-			result.data = this.prepareMovie(result.data);
+			result.data = this.formatMovie(result.data);
 		}
 
 		return result;
 	}
 
-	async search(query: string): Promise<ApiResults<MovieSearchResults>> {
+	async search(query: string): Promise<ApiResult<MovieSearchResults>> {
 		const params = new URLSearchParams({ query });
 		const result = await this.get<MovieSearchResults>(`${API_BASE_URL}/search/movie?${params}`);
 		if (result.data) {
-			result.data = {
-				...result.data,
-				results: result.data.results.map(this.prepareMovie.bind(this))
-			};
+			result.data.results = this.formatMovies(result.data.results);
 		}
 
 		return result;
 	}
 
-	private async get<T>(resource: string): Promise<ApiResults<T>> {
+	private async get<T>(resource: string): Promise<ApiResult<T>> {
 		if (resource in cache) {
-			return { data: cache[resource] as T };
+			return { data: JSON.parse(cache[resource]) as T };
 		}
 
 		const response = await fetch(resource, {
@@ -56,21 +58,23 @@ export class MoviesApi {
 			return { error: body };
 		}
 
-		cache[resource] = body;
+		cache[resource] = JSON.stringify(body);
 		return { data: body };
 	}
 
-	private prepareMovie<T extends { poster_path: string | null; backdrop_path: string | null }>(
-		movie: T
-	): T {
+	private formatMovie<T extends ImagePaths>(movie: T): T {
 		return {
 			...movie,
-			poster_path: this.prepareImageUrl(movie.poster_path),
-			backdrop_path: this.prepareImageUrl(movie.backdrop_path)
+			poster_path: this.buildImageUrl(movie.poster_path),
+			backdrop_path: this.buildImageUrl(movie.backdrop_path)
 		};
 	}
 
-	private prepareImageUrl(imagePath: string | null): string | null {
+	private formatMovies<T extends ImagePaths>(movies: T[]): T[] {
+		return movies.map(this.formatMovie.bind(this));
+	}
+
+	private buildImageUrl(imagePath: string | null): string | null {
 		return imagePath ? `${IMAGE_BASE_URL}${imagePath}` : null;
 	}
 }
